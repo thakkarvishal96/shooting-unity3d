ffb6e7da67f8f7a93e6c4dc683478f51fc11084d
vishal dev <vishal.unity3d@gmail.com>
2020-04-24 13:41:48 +0530

    1st

---
+++ Assets/script/shooting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour
+{
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+
+
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * bulletSpeed;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            Fire();
+    }
+}
+
